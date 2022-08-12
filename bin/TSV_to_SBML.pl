6b268ba251e86f8f0013a355ecde3c6da4ec9963
smoretti <sebastien.moretti@sib.swiss>
2022-08-12 16:46:04 +0200

    Add TODOs

--- bin/TSV_to_SBML.pl
+++ bin/TSV_to_SBML.pl
@@ -161,6 +161,17 @@ if ( $TSV_directory ){
     }
 
 
+#TODO SBML RNG Validator
+#     Error Line 192608 Column 27: element "fbc:listOfObjectives" missing required attribute "fbc:activeObjective"
+#
+#TODO SBML validator
+#     ErrorThe value of attribute 'fbc:chemicalFormula' on the SBML <species> object must be set to a string consisting only of atomic names or user defined compounds and their occurrence. Reference: L3V1 Fbc V3 Section 3.4 Encountered '*' when expecting a capital letter.The chemicalFormula 'C23H41N2O9PS*' has incorrect syntax.
+#       => replace * by R but how many ? Or no fbc:chemicalFormula in that case ! (check mass)
+#
+#TODO -validate         Validate SBML (check consistency)
+#     option returns *Segmentation fault*
+#
+#TODO MEMOTE to do
 #TODO allow a model to be kegg-oriented for example:
 #     without conflicts the id will be kegg ids
 
